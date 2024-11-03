2dc411555caf208d50e84d13e7b98fde2985f3c5
barbara-morochuico-vinci <barbara.morochuico@student.vinci.be>
2024-11-03 20:43:09 +0100

    new:ex2.5-destructuring

--- exercises/ex2.5/src/components/App/index.tsx
+++ exercises/ex2.5/src/components/App/index.tsx
@@ -19,7 +19,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Counter/>
+        <Counter
+        title='yo'
+        messageAfter10Clicks='You are a master in the art of clicking !'/>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
--- exercises/ex2.5/src/components/Counter/index.tsx
+++ exercises/ex2.5/src/components/Counter/index.tsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 
-const Counter = () => {
+interface CounterProps {
+    title : string,
+    messageAfter10Clicks : string
+}
+
+const Counter = ({title, messageAfter10Clicks} : CounterProps) => {
     const [count, setCount] = useState(0)
-    
-    return <button onClick={() => setCount((count) => count + 1)}>
+
+    return <div> 
+       <h4>{title}</h4>
+    <button onClick={() => setCount((count) => count + 1)}>
     count is {count}
   </button>
+  {count >= 10 ? <p>{messageAfter10Clicks}</p> : null}
+  </div>
 };
 
 export default Counter;
