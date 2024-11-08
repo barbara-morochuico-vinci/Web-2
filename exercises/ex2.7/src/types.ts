fa4c9e982090934b037f3eff867700eb4016f1c8
barbara-morochuico-vinci <barbara.morochuico@student.vinci.be>
2024-11-08 09:19:26 +0100

    new: ex2.7

---
+++ exercises/ex2.7/src/types.ts
@@ -0,0 +1,10 @@
+interface Film {
+    title : string,
+    director : string,
+    duration : number,
+    image_url ?: string,
+    description ?: string,
+    budget ?: number
+}
+
+export type {Film}
