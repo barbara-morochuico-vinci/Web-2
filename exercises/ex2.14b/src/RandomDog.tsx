0ecd7e2ba29241dad5245e4bd253759e2318cc75
barbara-morochuico-vinci <barbara.morochuico@student.vinci.be>
2024-11-29 09:55:47 +0100

    new: ex2.14b-refactor

---
+++ exercises/ex2.14b/src/RandomDog.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+interface Dog {
+    message: string;
+    status: string;
+  }
+
+const RandomDog = () => {
+    const [dog, setDog] = useState<Dog | undefined>(undefined);
+    useEffect(() => {
+        fetchDogs();
+      }, []);
+
+      const fetchDogs = async () => {
+        try {
+          const reponse = await fetch("https://dog.ceo/api/breeds/image/random");
+          if (!reponse.ok)
+            throw new Error(
+              `fetch error : ${reponse.status} : ${reponse.statusText}`
+            );
+            const data = await reponse.json();
+            setDog({
+              message: data.message ?? "No doggo found",
+              status: data.status ?? "Error"
+            });
+        } catch (error) {
+          console.error("HomePage::error: ", error);
+        }
+      }
+
+      if (!dog) {
+        return <p>Loading...</p>;
+      }
+    return (
+        <div>
+            <h3>Random dog</h3>
+            <img src={dog.message} alt="Random dog" style={{ maxHeight: 300 }} />
+        </div>
+    );
+}
+
+export default RandomDog;
